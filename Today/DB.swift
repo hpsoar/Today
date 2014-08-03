ad8e0372c932b0885d626b8d575614dee589f7cc
Aldrich Huang <hpsoar@gmail.com>
2014-08-03 16:17:55 +0800

    added uis

---
+++ Today/DB.swift
@@ -0,0 +1,13 @@
+//
+//  DB.swift
+//  Today
+//
+//  Created by Aldrich Huang on 8/3/14.
+//  Copyright (c) 2014 beacon. All rights reserved.
+//
+
+import UIKit
+
+class DB: NSObject {
+   
+}
---
+++ Today/TodayItemCell.swift
@@ -0,0 +1,29 @@
+//
+//  TodayItemCell.swift
+//  Today
+//
+//  Created by Aldrich Huang on 8/2/14.
+//  Copyright (c) 2014 beacon. All rights reserved.
+//
+
+import UIKit
+
+class TodayItemCell: UITableViewCell {
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
