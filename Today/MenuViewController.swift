8896a3b0c480a2a5855b804861ceac36bbe1c209
hpsoar <hpsoar@gmail.com>
2014-08-18 21:15:03 +0800

    refine animation

---
+++ Today/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  Today
+//
+//  Created by HuangPeng on 8/18/14.
+//  Copyright (c) 2014 beacon. All rights reserved.
+//
+
+import Cocoa
+
+class MenuViewController: UIViewController {
+
+}
